097c3ecdac56c1e6519ddbfca9a44defa444944f
Marek3008 <banasmre@gmail.com>
2022-10-18 15:24:20 +0200

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	string word[] = { "marek", "marek", "peter", "adam", "adam", "adam" };
+	string input;
+	int size = *(&word + 1) - word;
+	int countName = 0;
+	
+	cout << "ake meno chces spocitat? ";
+	cin >> input;
+
+	for (int i = 0; i <= size; i++) {
+
+		if (word[i] == input) {
+			countName += 1;
+		}
+		if (i == size) {
+			cout << input << " sa v liste nachadza " << countName << " krat." << endl;
+		}
+
+	}
+
+	return 0;
+}
